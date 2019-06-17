d52572ea99207cc625b7fe262933e98177a33180
brukk <36897786+brukk@users.noreply.github.com>
2019-06-17 15:08:13 -0400

    BI project phaase III SP added

--- SharePointTestSoln/DW_Codes/DimJunk_Full.sql
+++ SharePointTestSoln/DW_Codes/DimJunk_Full.sql
@@ -1,28 +1,32 @@
-CREATE PROCEDURE usp_DimJunk_Populator AS
+Alter PROCEDURE usp_DimJunk_Populator AS
 
 BEGIN
+
 declare @dimjunk table
-	 (JunkSK int identity(1,1),NKey INT, [Name] varchar(200), [Value] varchar(200), SourceName varchar(100))
-Insert INTO @dimjunk (NKey, Name, Value,SourceName)
+	 (JunkSK int identity(1,1),NKey INT, [Name] varchar(200), [Value] varchar(200))
+Insert INTO @dimjunk (NKey, Name, Value)
 /*the first row has to be a null handling row to record null values but that has contextual meaning*/
 select * from (
-select -1 Nkey,'NA' Name,'Unknown' Value,'NA' SourceName
+select -1 Nkey,'NA' Name,'Unknown' Value
 ) x
 
-Insert INTO @dimjunk (NKey, Name, Value, SourceName)
-Select 1,'Status','5','SharePoint'
+Insert INTO @dimjunk (NKey, Name, Value)
+Select 1,'Status','5'
 UNION
-Select 21, 'RevisionNumber', '1', 'SharePoint'
+Select 21, 'RevisionNumber', '1'
 UNION
-Select 41, 'ShipMethod', 'CargoTransport 5', 'SharePoint'
+Select 41, 'ShipMethod', 'Cargo Transport 5'
 UNION
-Select 61,'OnlineOrderFlag','1','SharePoint'
+Select 61,'OnlineOrderFlag','1'
 UNION
-Select 62,'OnlineOrderFlag','0','SharePoint'
+Select 62,'OnlineOrderFlag','0'
+Union
+Select 81, 'SourceName', 'MySQL'
+UNION
+Select 82, 'SourceName', 'SharePoint'
 
-select * into dimJunk from @dimjunk
-select * from dimJunk
 
+select * into DimJunk from @dimjunk
 
 END
 
--- SharePointTestSoln/DW_Codes/FactSales_Final.sql
+++ SharePointTestSoln/DW_Codes/FactSales_Final.sql
@@ -1,23 +1,64 @@
 ALTER procedure usp_FactSales_Populator AS
+
 BEGIN
+
+IF EXISTS (
+	SELECT 
+		OH.SalesOrderId, OD.SalesOrderDetailID, OD.OrderQty, OD.Unitprice, OD.UnitPriceDiscount, OD.LineTotal , DDOrder.dimDateKey , DDShip.dimDateKey , 
+	    DDDue.dimDateKey , OH.SalesOrderNumber, OH.PurchaseOrderNumber, OH.SubTotal, OH.TaxAmt, OH.Freight, OH.TotalDue, 
+		DA.AddressSK, DC.CustomerSK, DP.ProductSK, D3.JunkSK, D2.JunkSK, D4.JunkSK, D.JunkSK , D5.JunkSK  
+	FROM 
+		stgSalesOrderDetail OD JOIN 
+		stgSalesOrderHeader OH ON (OD.SalesOrderId = OH.SalesOrderId ) JOIN 
+		DimAddress DA ON (DA.AddressID = OH.BillToAddressID AND DA.SourceName = OH.SourceName AND DA.IsActive = 1 ) JOIN 
+		DimCustomer DC ON (DC.CustomerID = OH.CustomerID AND DC.SourceName = OH.SourceName AND DC.IsActive = 1 ) JOIN 
+		DimProduct DP ON (DP.ProductID = OD.ProductId AND DP.SourceName = OD.sourceName ) JOIN 
+		DimDate DDOrder ON OH.OrderDate = DDOrder.[Date] JOIN 
+		DimDate DDShip ON OH.ShipDate = DDShip.[Date] JOIN 
+		DimDate DDDue ON OH.DueDate = DDDue.[Date] JOIN 
+		DimJunk D on D.[Value] = OH.OnlineOrderFlag AND D.[Name] ='OnlineOrderFlag' JOIN 
+		DimJunk D2 on D2.[Value] = OH.RevisionNumber AND D2.[Name] ='RevisionNumber' JOIN 
+		DimJunk D3 on D3.[Value] = OH.[status]  AND D3.[Name] = 'status' JOIN
+		DimJunk D4 on D4.[Value] = OH.ShipMethod AND D4.[Name] = 'shipMethod' JOIN
+		DimJunk D5 on D5.[Value] = OH.SourceName AND D5.[Name] = 'SourceName'
+
+	EXCEPT
+
+	SELECT SalesOrderID, SalesOrderDetailID, OrderQty, UnitPrice, UnitPriceDiscount, LineTotal,
+		   OrderDateKey, ShipDateKey, DueDateKey, SalesOrderNumber, PurchaseOrderNumber, SubTotal, TaxAmt, Freight, TotalDue, 
+		   AddressSK, CustomerSK, ProductSK , StatusKey, RevisionNumberKey, ShipMethodKey, OnlineOrderFlagKey , SourceNameKey 
+    FROM FactSales
+
+)
 
 INSERT INTO FactSales (SalesOrderID, SalesOrderDetailID, OrderQty, UnitPrice, UnitPriceDiscount, LineTotal,
 					   OrderDateKey, ShipDateKey, DueDateKey, SalesOrderNumber, PurchaseOrderNumber, SubTotal, TaxAmt, Freight, TotalDue, 
-					   AddressSK, CustomerSK, ProductSK )
-
+					   AddressSK, CustomerSK, ProductSK , StatusKey, RevisionNumberKey, ShipMethodKey, OnlineOrderFlagKey , SourceNameKey
+)
 select  OH.SalesOrderId, OD.SalesOrderDetailID, OD.OrderQty, OD.Unitprice, OD.UnitPriceDiscount, OD.LineTotal , DDOrder.dimDateKey , DDShip.dimDateKey , 
 	   DDDue.dimDateKey , OH.SalesOrderNumber, OH.PurchaseOrderNumber, OH.SubTotal, OH.TaxAmt, OH.Freight, OH.TotalDue, 
 	   DA.AddressSK, DC.CustomerSK, DP.ProductSK
+	   , D3.JunkSK, D2.JunkSK, D4.JunkSK, D.JunkSK , D5.JunkSK
 -- SELECT * 
 from 
-stgSalesOrderDetail OD join 
+stgSalesOrderDetail OD JOIN 
 stgSalesOrderHeader OH ON (OD.SalesOrderId = OH.SalesOrderId ) JOIN 
 DimAddress DA ON (DA.AddressID = OH.BillToAddressID AND DA.SourceName = OH.SourceName AND DA.IsActive = 1 ) JOIN 
 DimCustomer DC ON (DC.CustomerID = OH.CustomerID AND DC.SourceName = OH.SourceName AND DC.IsActive = 1 ) JOIN 
 DimProduct DP ON (DP.ProductID = OD.ProductId AND DP.SourceName = OD.sourceName ) JOIN 
-DimDate DDOrder ON OH.OrderDate = DDOrder.Date JOIN 
-DimDate DDShip ON OH.ShipDate = DDShip.Date JOIN 
-DimDate DDDue ON OH.DueDate = DDDue.Date
+DimDate DDOrder ON OH.OrderDate = DDOrder.[Date] JOIN 
+DimDate DDShip ON OH.ShipDate = DDShip.[Date] JOIN 
+DimDate DDDue ON OH.DueDate = DDDue.[Date] JOIN 
+DimJunk D on D.[Value] = OH.OnlineOrderFlag AND D.[Name] ='OnlineOrderFlag' JOIN 
+DimJunk D2 on D2.[Value] = OH.RevisionNumber AND D2.[Name] ='RevisionNumber' JOIN 
+DimJunk D3 on D3.[Value] = OH.[status]  AND D3.[Name] = 'status' JOIN
+DimJunk D4 on D4.[Value] = OH.ShipMethod AND D4.[Name] = 'shipMethod' JOIN
+DimJunk D5 on D5.[Value] = OH.SourceName AND D5.[Name] = 'SourceName' 
+
+LEFT JOIN FactSales FS
+ON OH.SalesOrderId = FS.SalesOrderID AND OD.SalesOrderDetailID = FS.SalesOrderDetailID
+WHERE  FS.SalesOrderDetailID IS NULL
+
 
 END
 
